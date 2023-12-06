276909358dc4ce17ac523e4a67c3c86dca36f60d
Jonguk Lee <looke2930@gmail.com>
2023-12-06 17:50:01 +0900

    FIX : 현장 추가 요구사항에 대한 코드

--- debounce.js
+++ debounce.js
@@ -16,8 +16,54 @@ const delayFunction = () => {
   debounceCount.innerText = `${delayCount}`;
 }
 
-const debounce = (func, wait = 500, immediate = false) => {
+// const debounce = (func, wait = 500, immediate = false) => {
+//   let timeoutId;
+//   let promise;
+//   let resolvePromise; 
+
+//   const setPromise = () => {
+//     if(resolvePromise) {
+//       resolvePromise();
+//       resolvePromise = null;
+//     }
+//   }
+
+//   const debounced = () => {
+//     const later = () => {
+//       timeoutId = null;
+//       if (!immediate) {
+//         func();
+//         setPromise();
+//       }
+//     }
+//     clearTimeout(timeoutId);
+  
+//     if (!promise) {
+//       promise = new Promise(resolve => {
+//         resolvePromise = resolve;
+//       });
+//     }
+//     if (immediate) {
+//       func();
+//       setPromise();
+//     }
+//     timeoutId = setTimeout(later, wait);
+    
+//     return promise;
+//   };
+  
+//   debounced.cancel = () => {
+//     clearTimeout(timeoutId);
+//     timeoutId = null;
+//     setPromise();
+//   };
+
+//   return debounced;
+// }
+
+const debounce = (func, wait = 500, immediate = false, maxWait = 1000) => {
   let timeoutId;
+  let maxTimeoutId;// 새로운 변수 선언해줌
   let promise;
   let resolvePromise; 
 
@@ -35,8 +81,11 @@ const debounce = (func, wait = 500, immediate = false) => {
         func();
         setPromise();
       }
+      clearTimeout(maxTimeoutId);
+      maxTimeoutId = null; 
     }
     clearTimeout(timeoutId);
+    clearTimeout(maxTimeoutId);
   
     if (!promise) {
       promise = new Promise(resolve => {
@@ -48,6 +97,11 @@ const debounce = (func, wait = 500, immediate = false) => {
       setPromise();
     }
     timeoutId = setTimeout(later, wait);
+    maxTimeoutId = setTimeout(()=>{
+      if(timeoutId){
+        later();
+    }}, maxWait);
+    
     return promise;
   };
   
@@ -60,8 +114,9 @@ const debounce = (func, wait = 500, immediate = false) => {
   return debounced;
 }
 
-let delayDebounced = debounce(delayFunction, 500);
-let immediateDebounced = debounce(delayFunction, 500, true);
+
+let delayDebounced = debounce(delayFunction, 500,false, 1000);
+let immediateDebounced = debounce(delayFunction, 500, true, 1000);
 
 btn.addEventListener("click", () => {
   clickHandler();
